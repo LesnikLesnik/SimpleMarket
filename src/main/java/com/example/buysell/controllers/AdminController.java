ce4d21a802d016a5ab8a552d16e0cca9b2b7d650
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-10-03 18:03:13 +0300

    изменение параметров безопасности и класса AdminController

--- src/main/java/com/example/buysell/controllers/AdminController.java
+++ src/main/java/com/example/buysell/controllers/AdminController.java
@@ -3,6 +3,7 @@ package com.example.buysell.controllers;
 
 import com.example.buysell.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
+@PreAuthorize("hasAuthority('ROLE_ADMIN')")
 public class AdminController {
     private final UserService userService;
 
@@ -20,8 +22,9 @@ public class AdminController {
         return "admin";
     }
 
-    @PostMapping("/user/ban/{id}")
+    @PostMapping("/admin/user/ban/{id}")
     public String userBan(@PathVariable("id") Long id) {
         userService.banUser(id);
+        return "redirect:/admin";
     }
 }
