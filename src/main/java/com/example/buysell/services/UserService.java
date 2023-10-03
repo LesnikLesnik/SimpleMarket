6d98cb20c2e1446cbc506fdc940190af4d26976d
LesnikLesnik <133019465+LesnikLesnik@users.noreply.github.com>
2023-10-03 16:49:40 +0300

    Добавлена возможность банить пользователя и получение всего списка пользователей

--- src/main/java/com/example/buysell/services/UserService.java
+++ src/main/java/com/example/buysell/services/UserService.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 //прописываем базовую логику регистрации
 @Service
 @Slf4j
@@ -19,12 +21,25 @@ public class UserService {
 
     public boolean createUser(User user) { //регистрация пользователя
         String email = user.getEmail();
-        if (userRepo.findByEmail(email)!= null) return false;
+        if (userRepo.findByEmail(email) != null) return false;
         user.setActive(true);
         user.setPassword(passwordEncoder.encode(user.getPassword())); //шифруем пароль
-        user.getRoles().add(Role.USER);
+        user.getRoles().add(Role.ADMIN);
         log.info("Saving new User with email: {}", email);
         userRepo.save(user);
         return true;
     }
+
+    public List<User> list() {
+        return userRepo.findAll();
+    } //передаем список всем пользователей
+
+    public void banUser(Long id) {
+        User user = userRepo.findById(id).orElse(null);
+        if (user != null) {
+            user.setActive(false);
+            log.info("Ban user with id = {}; email: {}", user.getId(), user.getEmail());
+        }
+        userRepo.save(user);
+    } //устанавливаем активность пользователя на false, что деактивирует его возможность делать что-то
 }
